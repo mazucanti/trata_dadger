11121fb1c839d95dc906c84a7ebbc621cf0aaf77
mazucanti <daniel.mazucanti.ismart@gmail.com>
2020-03-16 16:19:45 -0300

    Estrutura básica do módulo de importaçao escrita. Extração dos DADGERs e checagem de formatação

--- importacao.py
+++ importacao.py
@@ -3,3 +3,11 @@ import pandas as pd
 
 
 #A codificação do arquivo é ISO-8859-1
+
+def importa_dadger():
+    rv = int(input("Revisão atual: "))
+
+    local = Path("DECOMP/DADGER.RV%d" % rv)
+
+    dadger = pd.read_csv(local, encoding="ISO-8859-1")
+    return dadger
