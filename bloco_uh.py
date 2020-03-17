19cb4c0eafaff484921b33345e33908060d8d992
mazucanti <daniel.mazucanti.ismart@gmail.com>
2020-03-17 17:58:27 -0300

    Geração do DataFrame tratado a partir do bloco UH implementado

--- bloco_uh.py
+++ bloco_uh.py
@@ -5,7 +5,7 @@ import re
 
 def importa_dadger():
     # rv = int(input("Revisão atual: "))
-    rv = 2
+    rv = 1
     bloco_uh = []
     uh = re.compile("^UH.*")
 
@@ -14,12 +14,32 @@ def importa_dadger():
     with open(local_dadger, 'r', encoding="ISO-8859-1") as dadger:
         for linha in dadger:
             if uh.match(linha) != None:
+                linha = linha.replace(".",",")
                 bloco_uh.append(linha)
     
     return bloco_uh
 
 
-uh = importa_dadger()
-with open("jaaj.txt", 'w') as fp:
+def cria_df():
+    uh = importa_dadger()
+    dados = []
+    colunas = ["UH", "Nº Usina", "REE", "Vol. Arm. Inicial %", "Vaz. Def. Min", "Evap", "Estágio", "Vol. Morto Inicial", "Lim. Sup. Vertimento", "Balanço Hídrico Fio D'água"]
     for linha in uh:
-        fp.write(linha)
+        if len(linha)>41:
+            to_df = [linha[0:2].strip(), linha[4:7].strip(), linha[9:11].strip(), linha[14:24].strip(), linha[24:34].strip(),
+                     linha[39].strip(), linha[44:46].strip(), linha[49:59].strip(), linha[59:69].strip(), linha[69].strip()]
+        else:
+            to_df = [linha[0:2].strip(), linha[4:7].strip(), linha[9:11].strip(), linha[14:24].strip(), linha[24:34].strip(),
+                     linha[39].strip(), linha[44:46].strip()]
+        dados.append(to_df)
+    df_uh = pd.DataFrame(dados, columns=colunas)
+    return df_uh
+
+def trata_df():
+    df = cria_df()
+    df.drop(["UH"], axis=1, inplace=True)
+    df.set_index("Nº Usina", inplace=True)
+    return df
+
+df = trata_df()
+df.to_csv("debug.csv")
