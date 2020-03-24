647cbad0995cc06e7da5c6594dc35b7e3dce4cbb
mazucanti <daniel.mazucanti.ismart@gmail.com>
2020-03-24 17:51:35 -0300

    Corte de blocos válidos do manual implementada

--- corta-blocos.py
+++ corta-blocos.py
@@ -3,15 +3,13 @@ import pandas as pd
 from pathlib import Path
 
 
-MN = ['TE', 'PT', 'SB', 'UH', 'CT', 'UE', 'ME', 'DP', 'CD', 'TD',
-      'BE', 'PQ', 'IT', 'IA', 'RC', 'TX', 'PE', 'GP', 'NI', 'PD',
-      'DT', 'MP', 'MT', 'FD', 'VE', 'VM', 'DF', 'RE', 'LU', 'FU',
-      'FT', 'FI', 'VI', 'QI', 'AC', 'RV', 'FP', 'FQ', 'IR', 'CI',
-      'CE', 'RS', 'QA', 'QV', 'FC', 'EA', 'ES', 'TI', 'RQ', 'EZ',
-      'HA', 'LA', 'CA', 'HV', 'LV', 'CV', 'HQ', 'LQ', 'CQ', 'HE',
-      'CM', 'VR', 'DA', 'PU', 'VP', 'RT', 'SA', 'AR', 'EV', 'TG',
-      'GS', 'NL', 'GL']
-
+MN = ['TE', 'SB', 'UH', 'CT', 'UE', 'DP', 'CD',
+      'PQ', 'IA', 'TX', 'GP', 'NI', 'DT', 'MP',
+      'MT', 'FD', 'VE', 'RE', 'LU', 'FU', 'FT',
+      'FI', 'VI', 'AC', 'IR', 'CI', 'FC', 'TI',
+      'RQ', 'EZ', 'HV', 'LV', 'CV', 'HQ', 'LQ', 
+      'CQ', 'AR', 'EV']
+print(len(MN))
 local = Path("DECOMP/DADGER.RV2")
 for mn in MN:
     with open('debug/%s.txt' % mn, 'w') as fp:
@@ -21,6 +19,3 @@ for mn in MN:
                 if reg.match(linha) != None:
                     linha = linha.replace(".",",")
                     fp.write(linha)
-    
-
-        
